64e4be139147b83ca2a1b4786818ded9703710a6
Kota Watanabe <kota3316@gmail.com>
2020-05-03 17:01:43 -0700

    login page

---
+++ cypress/integration/QA/login.spec.js
@@ -0,0 +1,27 @@
+describe('homepage UI', () => {
+    beforeEach(function () {
+        cy.visit('/login')
+    })
+    it('shows error message', () => {
+        cy.get('form').within(($form) => {
+            cy.get('input').first().type('invalid@gmail.com')
+            cy.get('input').eq(1).type('invalidpassowrd')
+            cy.root().submit()
+        })
+        cy.get('.alert').contains('Invalid Credentials')
+    })
+    it('logged in and redirect to dashboard', () => {
+        cy.get('form').within(($form) => {
+            cy.get('input').first().type('valid@gmail.com')
+            cy.get('input').eq(1).type('123456')
+            cy.root().submit()
+        })
+        cy.url().should('include', 'dashboard')
+        cy.contains('Welcome valid name').should('be.visible')
+        
+    })
+    it('has a link to Sign up page', () => {
+        cy.contains('Sign Up').should('be.visible').click()
+        cy.url().should('include','register')
+    })
+})
